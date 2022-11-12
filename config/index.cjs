71175c6a0bb2082e3e1e7fa190ff532c9d8f2470
qinlingmingtian <117931984+qinlingmingtian@users.noreply.github.com>
2022-11-13 02:26:48 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -25,7 +25,9 @@ const USER_CONFIG = {
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'o6kBF6pxEOBYiqZkYxvqnpR6PKWE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '3-2',
+      LITERARY_PREFERENCE: '',
+      openUrl: 'https://wangxinleo.cn',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
