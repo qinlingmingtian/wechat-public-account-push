7b8119766547e1db5dfb982a35bc7b500b04c82d
qinlingmingtian <117931984+qinlingmingtian@users.noreply.github.com>
2022-11-13 02:50:50 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小懒猫',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o6kBF6pxEOBYiqZkYxvqnpR6PKWE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'o6kBF6pxEOBYiqZkYxvqnpR6PKWE',
+      useTemplateId: 'wUE9QkE0VNNhFRxJqiEc33Z8PCILem_eUEmIbRFgaQ8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '3-2',
       LITERARY_PREFERENCE: '',
